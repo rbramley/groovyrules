59b4f4830e3a992fbc349134676c84aeac6aa7f8
robnewsome <robnewsome@f5094825-0a61-4fad-8f51-a3f238f2bb2b>
2006-01-18 16:36:53 +0000

    Removed unused imports

--- groovyrules/src/org/groovyrules/core/RuleRuntimeImpl.java
+++ groovyrules/src/org/groovyrules/core/RuleRuntimeImpl.java
@@ -2,7 +2,6 @@ package org.groovyrules.core;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
--- groovyrules/src/org/groovyrules/core/StatelessRuleSessionImpl.java
+++ groovyrules/src/org/groovyrules/core/StatelessRuleSessionImpl.java
@@ -1,8 +1,6 @@
 package org.groovyrules.core;
 
 import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.rules.InvalidRuleSessionException;
