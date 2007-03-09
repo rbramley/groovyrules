6eadaaec1fade94182a34ac335f3282d3a8e4d81
robnewsome <robnewsome@f5094825-0a61-4fad-8f51-a3f238f2bb2b>
2007-03-09 14:55:41 +0000

    Added better test failure if path for rulebase configured incorrectly.

--- groovyrules/test/org/groovyrules/test/FileRuleTest.java
+++ groovyrules/test/org/groovyrules/test/FileRuleTest.java
@@ -1,5 +1,7 @@
 package org.groovyrules.test;
 
+import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 
 import javax.rules.RuleServiceProvider;
@@ -7,6 +9,8 @@ import javax.rules.RuleServiceProviderManager;
 import javax.rules.admin.RuleAdministrator;
 import javax.rules.admin.RuleExecutionSet;
 import javax.rules.admin.RuleExecutionSetProvider;
+
+import org.groovyrules.filerules.XMLRuleExecutionSetConfiguration;
 
 import junit.framework.TestCase;
 
@@ -17,14 +21,28 @@ import junit.framework.TestCase;
  */
 public class FileRuleTest extends TestCase {
 
+	public static final String RULESET_CONFIG_PATH = "/org/groovyrules/test/basic_rule_test.xml";
+	
+	/**
+	 * Gives user a decent warning if they haven't set a valid path in the
+	 * basic_rule_test.xml
+	 */
+	public void testPathSpecifiedProperlyInXML() throws Exception {
+
+		InputStream configStream = getClass().getResourceAsStream(RULESET_CONFIG_PATH);
+		XMLRuleExecutionSetConfiguration config = new XMLRuleExecutionSetConfiguration(configStream);
+		String ruleFile1Path = config.getRuleRoot() + config.getRuleFiles().get(0);
+		assertTrue("The path defined in the file '" + RULESET_CONFIG_PATH + "' cannot be found.\nPlease check the ruleroot is correct for your system - it is currently set to: " + config.getRuleRoot() + "\nNo rule file was found at: " + ruleFile1Path, new File(ruleFile1Path).canRead());
+		
+	}
+	
 	/**
 	 * This tests the createRuleExecutionSet by URI method
 	 */
 	public void testReadConfigurationFromURI() throws Exception {
 		
 		// First we contrive the URI (or a URL) from a resource off the classpath.
-		URL fileURL = getClass().getResource(
-		"/org/groovyrules/test/basic_rule_test.xml");
+		URL fileURL = getClass().getResource(RULESET_CONFIG_PATH);
 		
 		// Load the rule service provider of the implementation.
 		// Loading this class will automatically register this
@@ -49,5 +67,5 @@ public class FileRuleTest extends TestCase {
 		assertNotNull(res1);
 		
 	}
-	
+		
 }
